134b766bca1f31c8b15ac972700d3163caf41187
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2024-12-12 18:18:07 -0300

    estilização não ta pegando

--- nrfRetails/src/app/index.tsx
+++ nrfRetails/src/app/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-export default function Index() {
+import "../global.css";
+
+export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-lg">
-        oi
-      </Text>
-    </View>
-  )
+    <View className="flex-1 h-full items-center justify-center bg-black">
+      <Text className='text-red-500'>Não ta funcionando!</Text>
+      <StatusBar style="auto" />
+    </View >
+  );
 }
